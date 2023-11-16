5f857c40e520fe1eed75e74875ecb620dd566317
madza <madzack15@yahoo.se>
2023-11-16 11:16:26 +0100

    Zachery Home

--- app/src/main/java/se/ju/jana22oj/project_eclipse/HomeScreen.kt
+++ app/src/main/java/se/ju/jana22oj/project_eclipse/HomeScreen.kt
@@ -1,6 +1,9 @@
 package se.ju.jana22oj.project_eclipse
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,6 +21,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,21 +32,32 @@ import androidx.compose.ui.unit.dp
 fun HomeScreen(modifier: Modifier = Modifier){
     var homeTime by remember { mutableStateOf(true)}
 
+
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(painter = painterResource(id = R.drawable.my_back),
+            contentDescription = "BackgroundImage",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize())
+    }
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text("Welcome to Call of Duty Battleships",
+            Modifier.background(Color.Black),
             style = MaterialTheme.typography.bodyMedium,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
         Button(
             modifier = Modifier
                 .padding(vertical = 24.dp)
                 .height(50.dp)
                 .fillMaxWidth(fraction = 0.8f),
-            onClick = { homeTime = false },
+                onClick = { homeTime = false },
+                colors = ButtonDefaults.buttonColors(Color.Black)
 
 
             ) {
